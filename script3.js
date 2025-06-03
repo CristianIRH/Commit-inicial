ca58d4e8641bc262a0fe8fd7a4f50a4938a365a8
CristianIRH <cristianrh648@gmail.com>
2025-06-03 10:59:37 -0400

    commit 1
    validaciones

--- script3.js
+++ script3.js
@@ -1,2 +1,19 @@
 //telefono debe tener exactamente 8 digitos, no es obligatoria
+function validar(){
+    let eTelefono = document.getElementById('telefono')
+    let vTelefono = eTelefono.value
+    eErrorTelefono = document-getElementById('errorTelefono')
+    if (eTelefono <= 8){
+        console.log("Telefono registrado")
+    
+    }}
 //contrasenia debe tener mas de 5 caracteres, es obligatoria
+function validar(){
+    let eContraseña = documento.getElementById('password')
+    let vContraseña = eContraseña.value
+    eErrorContraseña = document.getElementById('errorPassword')
+    if (eContraseña >= 5){
+        console.log("Contraseña registrada")
+    }
+}
+
