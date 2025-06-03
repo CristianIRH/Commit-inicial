991f77dc8664ea12be0a3a3fa0db025842cad5ea
CristianIRH <cristianrh648@gmail.com>
2025-06-03 11:31:18 -0400

    commit final
    ultimo commit

--- script3.js
+++ script3.js
@@ -1,19 +1,33 @@
-//telefono debe tener exactamente 8 digitos, no es obligatoria
+registro = []
+
 function validar(){
-    let eTelefono = document.getElementById('telefono')
+    let eTelefono = document.getElementById('telefono') //telefono debe tener exactamente 8 digitos, no es obligatoria
     let vTelefono = eTelefono.value
     eErrorTelefono = document-getElementById('errorTelefono')
     if (eTelefono <= 8){
+        registro.push(eTelefono)
         console.log("Telefono registrado")
-    
-    }}
-//contrasenia debe tener mas de 5 caracteres, es obligatoria
-function validar(){
-    let eContraseña = documento.getElementById('password')
+
+    let eContraseña = documento.getElementById('password') //contrasenia debe tener mas de 5 caracteres, es obligatoria
     let vContraseña = eContraseña.value
     eErrorContraseña = document.getElementById('errorPassword')
     if (eContraseña >= 5){
+        registro.push(eContraseña)
         console.log("Contraseña registrada")
+    
+    }}
+
+function eliminar(){
+    if(eTelefono == eTelefono){
+        eTelefono.innerText
+        console.log("Registro eliminado")
+    }
+    if(eContraseña == eContraseña){
+        eContraseña.innerText
+        console.log("Contraseña eliminada")
+    }
     }
 }
+function almacenar(){
 
+}
